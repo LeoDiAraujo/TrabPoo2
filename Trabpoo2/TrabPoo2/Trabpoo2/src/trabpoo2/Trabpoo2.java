99cd338829c65f492a1875bc1a84bf00c2df71d5
Leandro Matheus <129981754+LeoDiAraujo@users.noreply.github.com>
2023-05-19 15:54:07 -0300

    Add files via upload

---
+++ Trabpoo2/TrabPoo2/Trabpoo2/src/trabpoo2/Trabpoo2.java
@@ -0,0 +1,63 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Trabpoo2 extends JFrame {
+    private JLabel userLabel;
+    private JTextField userText;
+    private JLabel passwordLabel;
+    private JPasswordField passwordText;
+    private JButton loginButton;
+    private JButton registerButton;
+
+    public Trabpoo2() {
+        super("Login");
+        setLayout(null);
+
+        userLabel = new JLabel("Usuário");
+        userLabel.setBounds(10, 10, 80, 25);
+        add(userLabel);
+
+        userText = new JTextField(20);
+        userText.setBounds(100, 10, 160, 25);
+        add(userText);
+
+        passwordLabel = new JLabel("Senha");
+        passwordLabel.setBounds(10, 40, 80, 25);
+        add(passwordLabel);
+
+        passwordText = new JPasswordField(20);
+        passwordText.setBounds(100, 40, 160, 25);
+        add(passwordText);
+
+        loginButton = new JButton("Enter");
+        loginButton.setBounds(10, 80, 80, 25);
+        add(loginButton);
+
+        registerButton = new JButton("Não tem login? Cadastre-se!");
+        registerButton.setBounds(100, 80, 200, 25);
+        add(registerButton);
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Envia as informações ao banco de dados
+            }
+        });
+
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Muda para a tela de Cadastro
+            }
+        });
+
+        setSize(320, 140);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Trabpoo2();
+    }
+}
